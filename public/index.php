05149b7202dd1326d085d7df44fc2790d4eea20d
Seu Nome <seu@email.com>
2023-10-13 22:41:34 -0300

    feat: include and requipe

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+include './teste.php'; #Continua a execução mesmo apos achar um erro
+// require './teste2.php';  #Para o codigo ao encontrar um erro
+
+
+$title = 'Home';
+$name = 'Elias';
+
+echo "$name  " . $sobrenome;
